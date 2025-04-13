74ccc0f4ceca5ba1990f2386aac11a2abe0e2687
prothegee <prothegee@proton.me>
2025-04-13 21:33:41 +0700

    adding example endpoint of '/api/check/status/config'

--- src/backend.rs
+++ src/backend.rs
@@ -2,21 +2,31 @@ use std::io;
 use may_minihttp::{HttpService, HttpServiceFactory, Request, Response};
 use crate::route::Router;
 
-pub struct BackendRoutes {
+pub struct BackendRouter {
     router: Router,
 }
 
-impl BackendRoutes {
+impl BackendRouter {
     pub fn new() -> Self {
         let mut router = Router::new();
 
-        router.add_route("/api/test/hello", crate::controllers::api::test::hello);
+        // /api/test*
+        {
+            router.add_route("/api/test/hello", 
+            crate::controllers::api::test::hello);
+        }
 
-        BackendRoutes { router }
+        // /api/check*
+        {
+            router.add_route("/api/check/status/config",
+                crate::controllers::api::check::status::config);
+        }
+
+        BackendRouter { router }
     }
 }
 
-impl HttpService for BackendRoutes {
+impl HttpService for BackendRouter {
     fn call(&mut self, req: Request, resp: &mut Response) -> io::Result<()> {
         self.router.handle_request(req, resp);
         Ok(())
@@ -26,9 +36,9 @@ impl HttpService for BackendRoutes {
 pub struct BackendAppFramework;
 
 impl HttpServiceFactory for BackendAppFramework {
-    type Service = BackendRoutes;
+    type Service = BackendRouter;
 
     fn new_service(&self, _id: usize) -> Self::Service {
-        BackendRoutes::new()
+        BackendRouter::new()
     }
 }
--- src/controllers/api/test.rs
+++ src/controllers/api/test.rs
@@ -1,11 +1,11 @@
 use yarte::Serialize;
 use may_minihttp::{Request, Response};
 
-use crate::{constants::http, types::message::TMessage};
+use crate::{constants::{content, http}, types::message::TMessage};
 
 #[allow(non_snake_case, unused_variables)]
 pub fn hello(req: Request, resp: &mut Response) {
-    resp.header("Content-Type: application/json");
+    resp.header(content::content_http_type::APPLICATION_JSON);
 
     match req.method() {
         http::method::GET => {
